97440c3c212dc289e7daca461c3e1896582b1e98
Doug Herman <douglash9882@gmail.com>
2021-08-31 19:24:21 -0500

    updated for chaning methods

--- user_assigment.py
+++ user_assigment.py
@@ -5,13 +5,19 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
+
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
+
     def display_user_balance(self):
         print(self.account_balance)
+
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
     
 
 doug = User('doug')
@@ -28,12 +34,13 @@ tony.make_deposit(100)
 tony.make_deposit(100)
 tony.display_user_balance
 
-kirby.make_deposit(500)
-kirby.make_withdrawal(100)
-kirby.make_withdrawal(50)
-kirby.make_withdrawal(20)
+kirby.make_deposit(500).make_deposit(100).make_deposit(50).make_withdrawal(100)
+# kirby.make_withdrawal(100)
+# kirby.make_withdrawal(50)
+# kirby.make_withdrawal(20)
 kirby.display_user_balance()
 doug.display_user_balance()
-kirby.transfer_money(doug, 50)
+kirby.transfer_money(doug, 50).transfer_money(tony, 100)
 doug.display_user_balance()
 kirby.display_user_balance()
+tony.display_user_balance()
